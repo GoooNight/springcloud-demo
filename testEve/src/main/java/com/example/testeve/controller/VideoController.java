f93a650c115394c8f508698c09d5365c39fb90b4
GoooNight <1274531789@qq.com>
2023-03-15 16:24:24 +0800

    两种不同的视频播放方式

--- testEve/src/main/java/com/example/testeve/controller/VideoController.java
+++ testEve/src/main/java/com/example/testeve/controller/VideoController.java
@@ -1,17 +1,19 @@
 package com.example.testeve.controller;
 
 import lombok.Cleanup;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.util.Optional;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 /**
  * @author 雨天留恋
@@ -19,10 +21,22 @@ import java.util.Optional;
  * 视频分段播放
  */
 @RestController
+@Slf4j
 public class VideoController {
 
-    @GetMapping("play")
-    public void getVideoRecourses(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    /**
+     * @param files:
+     * @param request:
+     * @param response:
+     * @return void
+     * @author 雨天留恋
+     * @description 视频分段播放
+     * @date 2023/3/15 16:01
+     */
+    @GetMapping("play/{file}")
+    public void getVideoRecourses(@PathVariable("file") String files, HttpServletRequest request,
+                                  HttpServletResponse response) throws IOException {
+        System.out.println(files);
         // 重置response
         response.reset();
         File file = new File("E:\\电影\\宠女青春白皮书\\[MagicStar] Oyabaka Seishun Hakusho EP03 [WEBDL] [1080p].mkv");
@@ -40,7 +54,6 @@ public class VideoController {
         long rangePre = 0;
         if (!range.isBlank()) {
             rangePre = Long.parseLong(range.substring(range.indexOf("=") + 1, range.indexOf("-")));
-
         }
         // 获取相应数据流
         @Cleanup ServletOutputStream outputStream = response.getOutputStream();
@@ -55,12 +68,40 @@ public class VideoController {
 
         response.setContentLength(len);
         //设置此次相应返回的数据范围(rangePre+len-1是因为浏览器开始指针会+1)
-        response.setHeader("Content-Range", "bytes "+rangePre+"-"+(rangePre+len-1)+"/"+fileLength);
+        response.setHeader("Content-Range", "bytes " + rangePre + "-" + (rangePre + len - 1) + "/" + fileLength);
         // 将这1MB的视频流响应给客户端
-        outputStream.write(bytes,0,len);
+        outputStream.write(bytes, 0, len);
         outputStream.close();
         randomAccessFile.close();
-        System.out.println("返回数据区间:【"+rangePre+"-"+(rangePre+len)+"】");
+        System.out.println("返回数据区间:【" + rangePre + "-" + (rangePre + len) + "】");
+    }
+    /**
+     * @param response:
+     * @param request:
+     * @return void
+     * @author 雨天留恋
+     * @description 视频直接播放，无法点击
+     * @date 2023/3/15 16:23
+     */
+    @GetMapping("playVideo")
+    public void playVideo(HttpServletResponse response, HttpServletRequest request) throws Exception {
+        URL url = new URL("http://stream4.iqilu.com/ksd/video/2020/02/17/97e3c56e283a10546f22204963086f65.mp4");
+        URLConnection connection = url.openConnection();
+        int contentLength = connection.getContentLength();
+        @Cleanup InputStream inputStream = connection.getInputStream();
+        byte[] bytes = new byte[1024*1024];
+        @Cleanup ServletOutputStream outputStream = response.getOutputStream();
+        int len = 0;
+        int count = 0;
+        response.setContentType("video/mp4");
+        response.setContentLength(contentLength);
+        while ((len = inputStream.read(bytes)) != -1) {
+            count ++ ;
+            outputStream.write(bytes, 0, len);
+            System.out.println("写入："+count);
+            response.flushBuffer();
+        }
+
     }
 
 }
